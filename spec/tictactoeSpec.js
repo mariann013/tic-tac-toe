b7c60c9899db4a7ef7589d0376a929a30bef2765
C2809186_Mari-Ann_Meling <c2809186@MB704.local>
2017-05-15 16:31:03 +0100

    player 1 can pick a symbol to play and also set player 2

--- spec/tictactoeSpec.js
+++ spec/tictactoeSpec.js
@@ -6,6 +6,16 @@ describe('Tic Tac Toe', function() {
     });
 
     it('starts with a board', function() {
-        expect(tictactoe.startGame()).toEqual([]);
+        expect(tictactoe.board.length).toEqual(3);
     });
+
+    it('expects a row to be empty', function() {
+        expect(tictactoe.board[0]).toEqual([0,0,0]);
+    });
+
+    it('expects player1 to pick a symbol', function() {
+        tictactoe.pickPlayer('x');
+        expect(tictactoe.player1).toEqual("x");
+        expect(tictactoe.player2).toEqual("o");
+    })
 });
--- src/tictactoe.js
+++ src/tictactoe.js
@@ -1,8 +1,24 @@
 function TicTacToe () {
 
-    this.board = [];
+    this.board = [  [0, 0, 0],
+                    [0, 0, 0],
+                    [0, 0, 0]  ];
+
+    var player1;
+    var player2;
 }
 
-TicTacToe.prototype.startGame  = function() {
+TicTacToe.prototype.startGame = function() {
     return this.board;
+}
+
+TicTacToe.prototype.pickPlayer = function(symbol) {
+
+    if (symbol === 'x') {
+        this.player1 = 'x';
+        this.player2 = 'o';
+    } else {
+        this.player1 = 'o';
+        this.player2 = 'y';
+    }
 }
