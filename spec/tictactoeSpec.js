1636a2b8ba8aa3ceab036136935603cc816bdf82
C2809186_Mari-Ann_Meling <c2809186@mb704.mynet>
2017-05-19 15:47:00 +0100

    allows to check for similarities in rows

--- spec/tictactoeSpec.js
+++ spec/tictactoeSpec.js
@@ -23,4 +23,23 @@ describe('Tic Tac Toe', function() {
         tictactoe.pickLocation(0, 0);
         expect(tictactoe.board[0][0]).toEqual('x');
     });
+
+    it('expects coordinates to be no higher than 2', function() {
+        expect(tictactoe.pickLocation(3, 0)).toEqual('Sorry try again');
+    });
+
+    it('should check to see if a row is filled with the same symbol' , function() {
+        tictactoe.pickPlayer('x');
+        tictactoe.pickLocation(0, 0);
+        tictactoe.pickLocation(1, 0);
+        tictactoe.pickLocation(2, 0);
+        console.log(tictactoe.board);
+        expect(tictactoe.checkRow()).toEqual('winner');
+    });
+
+    it('should check to see if a row is filled with different symbols' , function() {
+        tictactoe.pickPlayer('x');
+        tictactoe.pickLocation(0, 0);
+        expect(tictactoe.checkRow()).toEqual('no winner yet');
+    });
 });
--- src/tictactoe.js
+++ src/tictactoe.js
@@ -19,11 +19,31 @@ TicTacToe.prototype.pickPlayer = function(symbol) {
         this.player2 = 'o';
     } else {
         this.player1 = 'o';
-        this.player2 = 'y';
+        this.player2 = 'x';
     }
 };
 
-TicTacToe.prototype.pickLocation = function(x , y) {
-   this.board[y][x] = 'x';
-   console.log(this.board);
+TicTacToe.prototype.pickLocation = function(x, y) {
+    if (x > 2 || y > 2) {
+        return 'Sorry try again';
+    }
+    this.board[y][x] = 'x';
 };
+
+TicTacToe.prototype.checkRow = function() {
+    count = 0;
+    for(i=0; i<=2; i++) {
+        for(j=0; j<=2; j++) {
+            if (this.board[j][i] !== 0) {
+                count += 1;
+            }
+        }
+    }
+
+    if (count === 3) {
+        return "winner"
+    } else {
+        return 'no winner yet';
+    }
+};
+
