7341f1a7cd4f3d0f09e1661206f80d0def482b52
C2809186_Mari-Ann_Meling <c2809186@MB704.local>
2017-05-22 13:13:26 +0100

    can check rows for x's or o's

--- spec/tictactoeSpec.js
+++ spec/tictactoeSpec.js
@@ -20,6 +20,7 @@ describe('Tic Tac Toe', function() {
     });
 
     it('expects a player1 to choose a start co-ordinate', function() {
+        tictactoe.pickPlayer('x');
         tictactoe.pickLocation(0, 0);
         expect(tictactoe.board[0][0]).toEqual('x');
     });
@@ -28,13 +29,22 @@ describe('Tic Tac Toe', function() {
         expect(tictactoe.pickLocation(3, 0)).toEqual('Sorry try again');
     });
 
-    it('should check to see if a row is filled with the same symbol' , function() {
+    it('should check to see if a row is filled with the x symbol' , function() {
         tictactoe.pickPlayer('x');
         tictactoe.pickLocation(0, 0);
         tictactoe.pickLocation(1, 0);
         tictactoe.pickLocation(2, 0);
         console.log(tictactoe.board);
-        expect(tictactoe.checkRow()).toEqual('winner');
+        expect(tictactoe.checkRow()).toEqual('x is the winner');
+    });
+
+    it('should check to see if a row is filled with the o symbol' , function() {
+        tictactoe.pickPlayer('o');
+        tictactoe.pickLocation(0, 1);
+        tictactoe.pickLocation(1, 1);
+        tictactoe.pickLocation(2, 1);
+        console.log(tictactoe.board);
+        expect(tictactoe.checkRow()).toEqual('o is the winner');
     });
 
     it('should check to see if a row is filled with different symbols' , function() {
--- src/tictactoe.js
+++ src/tictactoe.js
@@ -27,21 +27,32 @@ TicTacToe.prototype.pickLocation = function(x, y) {
     if (x > 2 || y > 2) {
         return 'Sorry try again';
     }
-    this.board[y][x] = 'x';
+    if (this.player1 === 'x') {
+        this.board[y][x] = 'x';
+    }
+    else {
+        this.board[y][x] = 'o';
+    }
 };
 
 TicTacToe.prototype.checkRow = function() {
-    count = 0;
+    x_count = 0;
+    o_count = 0
     for(i=0; i<=2; i++) {
         for(j=0; j<=2; j++) {
-            if (this.board[j][i] !== 0) {
-                count += 1;
+            if (this.board[j][i] === 'x') {
+                x_count += 1;
+            }
+            else if (this.board[j][i] === 'o') {
+                o_count += 1;
             }
         }
     }
 
-    if (count === 3) {
-        return "winner"
+    if (x_count === 3) {
+        return 'x is the winner';
+    } else if (o_count === 3) {
+        return 'o is the winner';
     } else {
         return 'no winner yet';
     }
